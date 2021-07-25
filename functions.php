876a27b51c395bcea5f4f3fdf9fccf4d63867ccb
LluisManas <manastorra@gmail.com>
2021-07-25 12:24:34 +0200

    main functions

---
+++ functions.php
@@ -0,0 +1,63 @@
+<?php
+
+$dbhost = 'localhost';
+$dbname = 'robinsnest';
+$dbuser = 'robinsnest';
+$dbpass = '';
+
+$connection = new msqli($dbhost, $dbname, $dbuser, $dbpass);
+if ($connection->connect_error) die('Fatal Error');
+
+
+function createTable($name, $query)
+{
+    queryMysql("CREATE TABLE IF NOT EXISTS $name($query)");
+    echo "Table '$name' created or already exists.<br>";
+}
+
+function queryMsql($query)
+{
+    global $connection;
+    $result = $connection->query($query);
+    if (!$result) die ("Fatal Error");
+    return $result;
+}
+
+function destroySession()
+{
+    $_SESSION = array();
+    if (session_id() != '' || isset($_COOKIE[session_name()])) {
+        setcookie(session_name(), '', time()-2592000, '/');
+    }
+    
+    session_destroy();
+}
+
+function sanitaizeString()
+{
+    global $connection;
+    $var = strip_tags($var); //remove all php & HTML tags
+    $var = htmlentities($var); //translates all html entities into code
+    if (get_magic_quotes_gpc()) {
+        $var = stripcslashes($var);
+    }
+
+    return $connection->real_escape_String($var);
+}
+
+function  showProfileUser($user)
+{
+    if(file_exists("$user.jpg")) {
+        echo "<img src='$user.jpg' style='float:left;'>";
+    }
+
+    $result = mysqlQuery("SELECT * FROM profiles WHERE user='$user'");
+
+    if ($result->num_rows) {
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        echo stripslashes($row['text']) . "<br style='clear:left;'><br>";
+    } else {
+        echo "<p>Nothing to see there, yet</p><br>";
+    }
+}
+?>
